d4c41db54afe79e757067bc0ecc80b912bc13c57
seungdobae <svvwac98@saltware.co.kr>
2025-03-17 14:53:27 +0900

    feat: instance 추가 8종

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -15,16 +15,31 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# local의 인스턴스 항목에 따라 ec2를 생성
-resource "aws_instance" "ec2_instances" {
-  for_each = { for idx, name in local.instance_names : idx => name }
+# Public_Subnet에 존재해야할 Instance 항목
+resource "aws_instance" "public_ec2_instances" {
+  for_each = { for idx, name in local.public_instance_names : idx => name }
 
   ami           = data.aws_ami.ubuntu.id
-  instance_type = each.key == "kubespray" ? "t3a.small" : "t3a.medium"
+  instance_type = each.key == "kubespray" ? "t3.small" : "t3.medium"
   key_name      = var.ec2_keypair_name
-  subnet_id     = each.key < 3 ? module.vpc.public_subnets[each.key] : module.vpc.private_subnets[each.key - 3]
 
-  associate_public_ip_address = each.key < 3
+  subnet_id     = module.vpc.public_subnets[each.key % 4]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = each.value
+  }
+}
+
+# Private_Subnet에 존재해야할 Instance 항목
+resource "aws_instance" "private_ec2_instances" {
+  for_each = { for idx, name in local.private_instance_names : idx => name }
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.medium"
+  key_name      = var.ec2_keypair_name
+
+  subnet_id     = module.vpc.private_subnets[each.key % 4]
 
   tags = {
     Name = each.value
--- terraform/local.tf
+++ terraform/local.tf
@@ -8,14 +8,16 @@ locals {
 
 # 인스턴스 목록
 locals {
-  instance_names = [
-    "kubespray",
-    "ingress-1",
-    "ingress-2",
+  private_instance_names = [
     "master-1",
     "master-2",
     "master-3",
     "worker-1",
-    "worker-2",
+    "worker-2"
+  ]
+  public_instance_names = [
+    "kubespray",
+    "ingress-1",
+    "ingress-2"  
   ]
 }
