c88e589b84845cc3ed55c5cf99ce2c7c2a26374c
seungdobae <svvwac98@saltware.co.kr>
2025-07-07 14:28:53 +0900

    아티팩트추가 및 인벤토리파일 이름 수정

--- kubespray/docker-run.sh
+++ kubespray/docker-run.sh
@@ -4,6 +4,7 @@ docker run \
 --detach \
 --restart=always \
 --mount type=bind,source="$(pwd)/inventory",dst=/kubespray/inventory \
+--mount type=bind,source="$(pwd)/artifacts",dst=/kubespray/artifacts \
 --mount type=bind,source=$HOME/.ssh,dst=/root/.ssh \
 --env ANSIBLE_HOST_KEY_CHECKING=False \
 quay.io/kubespray/kubespray:v2.28.0 sleep infinity
---
+++ kubespray/inventory/playbook-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ansible-playbook -i inventory.yaml --private-key /root/.ssh/id_rsa cluster.yml
