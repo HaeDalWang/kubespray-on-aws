a5f76872967b527bb014e5eece594a2098456c7e
seungdobae <svvwac98@saltware.co.kr>
2025-07-07 13:44:06 +0900

    path delete

--- kubespray/docker-run.sh
+++ kubespray/docker-run.sh
@@ -4,8 +4,6 @@ docker run \
 --detach \
 --restart=always \
 --mount type=bind,source="$(pwd)/inventory",dst=/kubespray/inventory \
---mount type=bind,source="$(pwd)/extra_playbooks",dst=/kubespray/extra_playbooks \
---mount type=bind,source="$(pwd)/artifacts",dst=/kubespray/artifacts \
 --mount type=bind,source=/root/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
 --env ANSIBLE_HOST_KEY_CHECKING=False \
 quay.io/kubespray/kubespray:v2.28.0 sleep infinity
