c849eeb0c0d20a02481fc35cedc9e8bc82b220d0
seungdobae <svvwac98@saltware.co.kr>
2025-07-07 13:42:44 +0900

    path 경로 잘못 수정

--- kubespray/docker-run.sh
+++ kubespray/docker-run.sh
@@ -3,9 +3,9 @@ docker run \
 --network=host \
 --detach \
 --restart=always \
---mount type=bind,source="$(pwd)/kubespray-on-aws/kubespray/inventory",dst=/kubespray/inventory \
---mount type=bind,source="$(pwd)/kubespray-on-aws/kubespray/extra_playbooks",dst=/kubespray/extra_playbooks \
---mount type=bind,source="$(pwd)/kubespray-on-aws/kubespray/artifacts",dst=/kubespray/artifacts \
+--mount type=bind,source="$(pwd)/inventory",dst=/kubespray/inventory \
+--mount type=bind,source="$(pwd)/extra_playbooks",dst=/kubespray/extra_playbooks \
+--mount type=bind,source="$(pwd)/artifacts",dst=/kubespray/artifacts \
 --mount type=bind,source=/root/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
 --env ANSIBLE_HOST_KEY_CHECKING=False \
 quay.io/kubespray/kubespray:v2.28.0 sleep infinity
