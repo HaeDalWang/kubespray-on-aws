9c0486b1cc8ae4d6ae5a90e9606cd93fe2fd4e84
seungdobae <svvwac98@saltware.co.kr>
2025-07-07 13:47:06 +0900

    path fix

--- kubespray/docker-run.sh
+++ kubespray/docker-run.sh
@@ -4,6 +4,6 @@ docker run \
 --detach \
 --restart=always \
 --mount type=bind,source="$(pwd)/inventory",dst=/kubespray/inventory \
---mount type=bind,source=$HOME/.ssh/id_rsa,dst=/root/.ssh/id_rsa \
+--mount type=bind,source=$HOME/.ssh,dst=/root/.ssh \
 --env ANSIBLE_HOST_KEY_CHECKING=False \
 quay.io/kubespray/kubespray:v2.28.0 sleep infinity
